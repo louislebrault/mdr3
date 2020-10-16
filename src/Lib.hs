2ccb207d2d74bce0d2d6a8928e7160b0d2d49d34
Louis Lebrault <louis.lebrault@gmail.com>
2020-10-16 15:19:12 +0200

    before  implementing participant list

--- src/Lib.hs
+++ src/Lib.hs
@@ -19,7 +19,7 @@ runMDR = runTCPServer Nothing "3000" talk
     talk h = do
 	line <- hGetLine h
 	putStrLn line
-	hPutStr h (parseLine line)
+	hPutStr h (parseLine [] line)
 	talk h 
 
 -- from the "network-run" package.
@@ -47,11 +47,11 @@ runTCPServer mhost port server = withSocketsDo $ do
         void $ forkFinally (server handle) (const $ gracefulClose conn 5000)
 
 
-parseLine :: String -> String
-parseLine line =
+parseLine :: [a] -> String -> String
+parseLine participants line =
   let command = getCommand line in
   case command of 
-    "KIKOO" -> handleKikoo line
+    "KIKOO" -> handleKikoo participants line
     "TAVU" -> handleTavu line
     _ -> "ERR \"Talk my language u foreigner\""
       
@@ -60,8 +60,8 @@ getCommand :: String -> String
 getCommand line = head (words line)
 
 -- TODO: not hard coded address and port
-handleKikoo :: String -> String
-handleKikoo line = "OKLM \"SuckMyLambdaCalculus\" ( / 127.0.0.1:3000?)*"
+handleKikoo :: [a] -> String -> String
+handleKikoo participants line = "OKLM \"SuckMyLambdaCalculus\" / 127.0.0.1:3000"
 
 handleTavu :: String -> String
 handleTavu line = "ACK"
